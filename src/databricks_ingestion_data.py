44eb0a4830042ef549a38452062dc6e445a0dcb4
Debora Kassem <deborakassem@live.com>
2026-05-10 16:24:31 -0300

    feat: filtragem dos dados para o período solicitado

--- src/databricks_ingestion_data.py
+++ src/databricks_ingestion_data.py
@@ -32,8 +32,6 @@ display(dbutils.fs.ls("/Volumes/workspace/nyc_taxi/landing_zone/"))
 
 # COMMAND ----------
 
-from functools import reduce
-from pyspark.sql import DataFrame
 from pyspark.sql import functions as f
 from pyspark.sql import types as t
 
@@ -41,24 +39,28 @@ from pyspark.sql import types as t
 
 LANDING_PATH = "/Volumes/workspace/nyc_taxi/landing_zone/"
 
-dfs = []
+df_final = None
+
 for month in ["01", "02", "03", "04", "05"]:
     path = f"{LANDING_PATH}yellow_tripdata_2023-{month}.parquet"
     df = spark.read.parquet(path)
-
-    if "Airport_fee" in df.columns:
-        df = df.withColumnRenamed("Airport_fee", "airport_fee")
-
     df = df.select(
-        f.col("VendorID").cast(t.LongType()),
+        f.col("VendorID").cast(t.LongType()).alias("vendor_id"),
         f.col("passenger_count").cast(t.IntegerType()),
         f.col("total_amount").cast(t.DoubleType()),
         f.col("tpep_pickup_datetime").cast(t.TimestampType()),
         f.col("tpep_dropoff_datetime").cast(t.TimestampType()),
     )
-    dfs.append(df)
 
-df_final = reduce(DataFrame.union, dfs)
+    if df_final is None:
+        df_final = df
+    else:
+        df_final = df_final.union(df)
+    
+df_final = df_final.filter(
+    (f.col("tpep_pickup_datetime") >= "2023-01-01") &
+    (f.col("tpep_pickup_datetime") <= "2023-05-31")
+)
 
 print(f"Total de linhas: {df_final.count():,}")
 df_final.printSchema()
@@ -72,34 +74,48 @@ df_final.write.format("delta").mode("overwrite").saveAsTable("workspace.nyc_taxi
 
 # MAGIC %sql
 # MAGIC -- Consultando a tabela para validacão
-# MAGIC SELECT * FROM workspace.nyc_taxi.yellow_trips LIMIT 10
+# MAGIC SELECT
+# MAGIC   *
+# MAGIC FROM
+# MAGIC   workspace.nyc_taxi.yellow_trips
+# MAGIC LIMIT 10
 
 # COMMAND ----------
 
 # MAGIC %sql
 # MAGIC -- Adiciona descrição da tabela
-# MAGIC ALTER TABLE workspace.nyc_taxi.yellow_trips 
-# MAGIC SET TBLPROPERTIES (
-# MAGIC     'comment' = 'Tabela com dados de corridas de táxi em Nova York para o ano de 2023.'
-# MAGIC );
+# MAGIC ALTER TABLE
+# MAGIC   workspace.nyc_taxi.yellow_trips
+# MAGIC SET TBLPROPERTIES
+# MAGIC   ('comment' = 'Tabela com dados de corridas de táxi em Nova York para o ano de 2023.');
 # MAGIC
 # MAGIC -- Adiciona descrição das colunas
-# MAGIC ALTER TABLE workspace.nyc_taxi.yellow_trips
-# MAGIC ALTER COLUMN VendorID
-# MAGIC COMMENT 'Código do fornecedor (1: Creative Mobile Technologies, 2: Curb Mobility, 6: Myle Technologies, 7: Helix).';
+# MAGIC ALTER TABLE
+# MAGIC   workspace.nyc_taxi.yellow_trips
+# MAGIC ALTER COLUMN
+# MAGIC   vendor_id
+# MAGIC   COMMENT 'Código do fornecedor (1: Creative Mobile Technologies, 2: Curb Mobility, 6: Myle Technologies, 7: Helix).';
 # MAGIC
-# MAGIC ALTER TABLE workspace.nyc_taxi.yellow_trips
-# MAGIC ALTER COLUMN passenger_count
-# MAGIC COMMENT 'Número de passageiros no veículo.';
+# MAGIC ALTER TABLE
+# MAGIC   workspace.nyc_taxi.yellow_trips
+# MAGIC ALTER COLUMN
+# MAGIC   passenger_count
+# MAGIC   COMMENT 'Número de passageiros no veículo.';
 # MAGIC
-# MAGIC ALTER TABLE workspace.nyc_taxi.yellow_trips
-# MAGIC ALTER COLUMN total_amount
-# MAGIC COMMENT 'Valor total cobrado do passageiro.';
+# MAGIC ALTER TABLE
+# MAGIC   workspace.nyc_taxi.yellow_trips
+# MAGIC ALTER COLUMN
+# MAGIC   total_amount
+# MAGIC   COMMENT 'Valor total cobrado do passageiro.';
 # MAGIC
-# MAGIC ALTER TABLE workspace.nyc_taxi.yellow_trips
-# MAGIC ALTER COLUMN tpep_pickup_datetime
-# MAGIC COMMENT 'Data e hora em que o taxímetro foi acionado.';
+# MAGIC ALTER TABLE
+# MAGIC   workspace.nyc_taxi.yellow_trips
+# MAGIC ALTER COLUMN
+# MAGIC   tpep_pickup_datetime
+# MAGIC   COMMENT 'Data e hora em que o taxímetro foi acionado.';
 # MAGIC
-# MAGIC ALTER TABLE workspace.nyc_taxi.yellow_trips
-# MAGIC ALTER COLUMN tpep_dropoff_datetime
-# MAGIC COMMENT 'Data e hora em que o taxímetro foi desligado.';
+# MAGIC ALTER TABLE
+# MAGIC   workspace.nyc_taxi.yellow_trips
+# MAGIC ALTER COLUMN
+# MAGIC   tpep_dropoff_datetime
+# MAGIC   COMMENT 'Data e hora em que o taxímetro foi desligado.';
